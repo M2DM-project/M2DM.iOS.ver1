547b747a88203aba6d51fc5c2dadf0a894b5bd43
JURI <80569323+juri123123@users.noreply.github.com>
2024-05-26 02:10:50 +0900

    design: 주석 제거

--- M2DM/M2DM/Presentation/Authentication/DataManager/AuthenticationProtocol.swift
+++ M2DM/M2DM/Presentation/Authentication/DataManager/AuthenticationProtocol.swift
@@ -35,28 +35,18 @@ extension Authentication: AuthenticationProtocol {
         let dataTask = request.serializingDecodable(KakaoResponse.self)
         
         switch await dataTask.result {
+            
         case .success(let value):
             guard let _ = await dataTask.response.response else {return KakaoResponse(access_token: "error", refresh_token: "error")}
-            //print("성공")
             return value
+            
         case .failure(_):
-            // 에러처리
+            // TODO: 에러 처리
+            #if DEBUG
             print("실패")
+            #endif
             return KakaoResponse(access_token: "error", refresh_token: "error")
         }
-//        return AF.request(url,
-//                          method: .get,
-//                          parameters: nil,
-//                          encoding: JSONEncoding.default)
-//        .validate() // 유효성 검증
-//        .publishDecodable(type: KakaoResponse.self) //디코딩
-//        //에러처리?? 여기 잘 모르겠음..
-//        .map { response in
-//            response.mapError { error in
-//                let backendError = response.data.flatMap { _ in }
-//                return error
-//            }
-//        }
-//        .eraseToAnyPublisher() // 최종 return 값을 AnyPublisher<DataResponse<KakaoResponse, Error>, Never>로 정리
+        
     }
 }
